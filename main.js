c6a4c1bbcf830ba2e7fd6afa2885587d275a6622
bodhismall <49545538+bodhismall@users.noreply.github.com>
2021-04-24 17:00:41 -0400

    Update main.js

--- main.js
+++ main.js
@@ -232,29 +232,29 @@ $('#submit-btn').click(function () {
 		// console.log(document.getElementById('intro-bar').style.width);
 		// document.getElementById('intro-bar').innerHTML= ((total / 60) * 100) + '%';
 		document.getElementById('results').innerHTML = '<b>You are Quiet Side!</b><br><br>\
-		Quiet Side people are tricky to understand, since it’s so easy for them to assume that introversion is the same as being shy, when, in fact, introverts are simply people who find it tiring to be around other people.\n\
+		Quiet Side people are _____.\n\
 <br><br>\
-I love this explanation of an introvert’s need to be alone:\n\
+___________\n\
 <br><br>\
-For introverts, to be alone with our thoughts is as restorative as sleeping, as nourishing as eating.\n\n\
+____________\n\n\
 <br><br>\
-Introverted people are known for thinking things through before they speak, enjoying small, close groups of friends and one-on-one time, needing time alone to recharge, and being upset by unexpected changes or last-minute surprises. Introverts are not necessarily shy and may not even avoid social situations, but they will definitely need some time alone or just with close friends or family after spending time in a big crowd.\
+____________\
 		';
 	}
     else if(total > 0) {
 		document.getElementById('results').innerHTML = '<b>You are Loud Side!</b><br><br>\
-		On the opposite side of the coin, people who are extroverted are energized by people. They usually enjoy spending time with others, as this is how they recharge from time spent alone focusing or working hard.\
+		____________\
 <br><br>\
-I like how this extrovert explains the way he/she gains energy from being around other people:\
+____________\
 <br><br>\
-When I am among people, I make eye contact, smile, maybe chat if there’s an opportunity (like being stuck in a long grocery store line). As an extrovert, that’s a small ‘ping’ of energy, a little positive moment in the day.';
+____________';
 	} else {
-		document.getElementById('results').innerHTML = '<b>You are ambiverted!</b><br><br>\
-		Since introverts and extroverts are the extremes of the scale, the rest of us fall somewhere in the middle. Many of us lean one way or the other, but there are some who are quite balanced between the two tendencies. These people are called ambiverts.\
+		document.getElementById('results').innerHTML = '<b>downstairs...</b><br><br>\
+		____________\
 <br><br>\
-So let’s look at how an ambivert compares.\
+____________\
 <br><br>\
-Ambiverts exhibit both extroverted and introverted tendencies. This means that they generally enjoy being around people, but after a long time this will start to drain them. Similarly, they enjoy solitude and quiet, but not for too long. Ambiverts recharge their energy levels with a mixture of social interaction and alone time.'
+____________'
 	}
 
 	// Hide the quiz after they submit their results
