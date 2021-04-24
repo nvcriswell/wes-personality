4bce7bba0d00a463a5e33d0e4a75c1b7ab02a429
izzywrubel <iwrubel@wesleyan.edu>
2021-04-24 16:40:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -235,6 +235,7 @@ For introverts, to be alone with our thoughts is as restorative as sleeping, as 
 <br><br>\
 Introverted people are known for thinking things through before they speak, enjoying small, close groups of friends and one-on-one time, needing time alone to recharge, and being upset by unexpected changes or last-minute surprises. Introverts are not necessarily shy and may not even avoid social situations, but they will definitely need some time alone or just with close friends or family after spending time in a big crowd.\
 		';
+	}
     else if(total > 0) {
 		document.getElementById('results').innerHTML = '<b>You are Loud Side!</b><br><br>\
 		On the opposite side of the coin, people who are extroverted are energized by people. They usually enjoy spending time with others, as this is how they recharge from time spent alone focusing or working hard.\
